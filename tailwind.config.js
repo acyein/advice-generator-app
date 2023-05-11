b34464a2f0631b9be570e2acc3e8b547db3dfabb
Chian Yein Ang <chianyein@gmail.com>
2023-05-11 15:34:52 +0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['index.html'],
+    theme: {
+        extend: {
+            fontFamily: {
+                'primary': ['Manrope', 'sans-serif']
+            },
+            colors: {
+                cyan: {
+                    'light': 'hsl(193, 38%, 86%)',
+                },
+                green: {
+                    'neon': 'hsl(150, 100%, 66%)'
+                },
+                blue: {
+                    'gray': 'hsl(217, 19%, 38%)',
+                    'gray-dark': 'hsl(217, 19%, 24%)',
+                    'dark': 'hsl(218, 23%, 16%)'
+                },
+            },
+        }
+    },
+    plugins: [],
+};
